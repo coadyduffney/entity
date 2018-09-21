05f9e7a3e98d77302315c5812fde4e3d0d84d20e
Coady Duffney <coadyduffney@gmail.com>
2018-09-21 10:33:50 -0300

    Added new galleries for 8.1 raids

--- js/main.js
+++ js/main.js
@@ -8,97 +8,121 @@ $(document).ready(function() {
 // Transition effect for navbar
 function animateNav() {
   if ($(this).scrollTop() > 500) {
-    $(".navbar").addClass("solid");
-    $(".navbar-brand").addClass("visible");
+    $('.navbar').addClass('solid');
+    $('.navbar-brand').addClass('visible');
   } else {
-    $(".navbar").removeClass("solid");
-    $(".navbar-brand").removeClass("visible");
+    $('.navbar').removeClass('solid');
+    $('.navbar-brand').removeClass('visible');
   }
 }
 
 // Initialize Lightgallery
-lightGallery(document.getElementById("legion-video-gallery"), {
+lightGallery(document.getElementById('legion-video-gallery'), {
   thumbnail: true,
-  exThumbImage: "data-exthumbimage",
+  exThumbImage: 'data-exthumbimage',
   loadYoutubeThumbnail: false
 });
 
-lightGallery(document.getElementById("uldir-video-gallery"), {
+lightGallery(document.getElementById('uldir-video-gallery'), {
   thumbnail: true,
-  exThumbImage: "data-exthumbimage",
+  exThumbImage: 'data-exthumbimage',
   loadYoutubeThumbnail: false
 });
 
-lightGallery(document.getElementById("temp-raid-video-gallery"), {
+lightGallery(document.getElementById('siege-of-zuldazar-video-gallery'), {
   thumbnail: true,
-  exThumbImage: "data-exthumbimage",
+  exThumbImage: 'data-exthumbimage',
+  loadYoutubeThumbnail: false
+});
+
+lightGallery(document.getElementById('crucible-of-storms-video-gallery'), {
+  thumbnail: true,
+  exThumbImage: 'data-exthumbimage',
   loadYoutubeThumbnail: false
 });
 
 // Hide & Show video galleries
-$("#uldir-link").on("click", function() {
-  $("#legion-video-gallery").css("display", "none");
-  $("#legion-text").css("display", "none");
-  $("#uldir-video-gallery").css("display", "block");
-  $("#uldir-text").css("display", "block");
-  $("#temp-raid-video-gallery").css("display", "none");
-  $("#temp-raid-text").css("display", "none");
+$('#uldir-link').on('click', function() {
+  $('#legion-video-gallery').css('display', 'none');
+  $('#legion-text').css('display', 'none');
+  $('#uldir-video-gallery').css('display', 'block');
+  $('#uldir-text').css('display', 'block');
+  $('#siege-of-zuldazar-video-gallery').css('display', 'none');
+  $('#siege-of-zuldazar-text').css('display', 'none');
+  $('#crucible-of-storms-video-gallery').css('display', 'none');
+  $('#crucible-of-storms-text').css('display', 'none');
 });
 
-$("#antorus-link").on("click", function() {
-  $("#legion-video-gallery").css("display", "block");
-  $("#legion-text").css("display", "block");
-  $("#uldir-video-gallery").css("display", "none");
-  $("#uldir-text").css("display", "none");
-  $("#temp-raid-video-gallery").css("display", "none");
-  $("#temp-raid-text").css("display", "none");
+$('#antorus-link').on('click', function() {
+  $('#legion-video-gallery').css('display', 'block');
+  $('#legion-text').css('display', 'block');
+  $('#uldir-video-gallery').css('display', 'none');
+  $('#uldir-text').css('display', 'none');
+  $('#siege-of-zuldazar-video-gallery').css('display', 'none');
+  $('#siege-of-zuldazar-text').css('display', 'none');
+  $('#crucible-of-storms-video-gallery').css('display', 'none');
+  $('#crucible-of-storms-text').css('display', 'none');
 });
 
-$("#temp-raid-link").on("click", function() {
-  $("#temp-raid-video-gallery").css("display", "block");
-  $("#temp-raid-text").css("display", "block");
-  $("#legion-video-gallery").css("display", "none");
-  $("#legion-text").css("display", "none");
-  $("#uldir-video-gallery").css("display", "none");
-  $("#uldir-text").css("display", "none");
+$('#siege-of-zuldazar-link').on('click', function() {
+  $('#siege-of-zuldazar-video-gallery').css('display', 'block');
+  $('#siege-of-zuldazar-text').css('display', 'block');
+  $('#legion-video-gallery').css('display', 'none');
+  $('#legion-text').css('display', 'none');
+  $('#uldir-video-gallery').css('display', 'none');
+  $('#uldir-text').css('display', 'none');
+  $('#crucible-of-storms-video-gallery').css('display', 'none');
+  $('#crucible-of-storms-text').css('display', 'none');
 });
 
-
-
+$('#crucible-of-storms-link').on('click', function() {
+  $('#crucible-of-storms-video-gallery').css('display', 'block');
+  $('#crucible-of-storms-text').css('display', 'block');
+  $('#siege-of-zuldazar-video-gallery').css('display', 'none');
+  $('#siege-of-zuldazar-text').css('display', 'none');
+  $('#legion-video-gallery').css('display', 'none');
+  $('#legion-text').css('display', 'none');
+  $('#uldir-video-gallery').css('display', 'none');
+  $('#uldir-text').css('display', 'none');
+});
 
 // Animate Smooth Scroll for Nav
-$("#scroll-arrow, #about-link").on("click", function() {
-  const about = $("#about").position().top;
-  $("html, body").animate({
+$('#scroll-arrow, #about-link').on('click', function() {
+  const about = $('#about').position().top;
+  $('html, body').animate(
+    {
       scrollTop: about
-    },700);
+    },
+    700
+  );
 });
 
-$("#video-link").on("click", function() {
-  const video = $("#videos").position().top;
-  $("html, body").animate({
+$('#video-link').on('click', function() {
+  const video = $('#videos').position().top;
+  $('html, body').animate(
+    {
       scrollTop: video
-    },700);
+    },
+    700
+  );
 });
 
-$("#apply-link").on("click", function() {
-  const apply = $("#apply").position().top;
-  $("html, body").animate({
+$('#apply-link').on('click', function() {
+  const apply = $('#apply').position().top;
+  $('html, body').animate(
+    {
       scrollTop: apply
-    },700);
+    },
+    700
+  );
 });
 
-$("#home-link").on("click", function() {
-  const home = $("#home").position().top;
-  $("html, body").animate({
+$('#home-link').on('click', function() {
+  const home = $('#home').position().top;
+  $('html, body').animate(
+    {
       scrollTop: home
-    },700);
+    },
+    700
+  );
 });
-
-
-
-
-
-
-
-
